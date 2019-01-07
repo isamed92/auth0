4b08eaaeb2203b45c047125935f918070a4b400f
Isa Notebook <isamed92@gmail.com>
2019-01-07 12:33:09 -0300

    add profile add w user access

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,6 +8,7 @@ import * as auth0 from 'auth0-js';
 @Injectable()
 export class AuthService {
 
+  userProfile: any;
   private _idToken: string;
   private _accessToken: string;
   private _expiresAt: number;
@@ -17,7 +18,7 @@ export class AuthService {
     domain: 'isamed92.auth0.com',
     responseType: 'token id_token',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
 
   constructor(public router: Router) {
@@ -87,6 +88,22 @@ export class AuthService {
     // Check whether the current time is past the
     // access token's expiry time
     return new Date().getTime() < this._expiresAt;
+  }
+
+
+
+  public getProfile(cb): void {
+    if (!this._accessToken) {
+      throw new Error('Access Token must exist to fetch profile');
+    }
+
+    const self = this;
+    this.auth0.client.userInfo(this._accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
   }
 
 }
